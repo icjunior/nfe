4373fb55b4298aa04672a5ca50318c1be318e780
Ismael Costa Junior <ismaelcostajunior@MacBook-Pro-de-Ismael.local>
2020-04-16 09:35:48 -0300

    Commit inicial

---
+++ src/test/java/br/com/bigsupermercados/nfe/NfeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bigsupermercados.nfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
